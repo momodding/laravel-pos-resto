967cedffc5f8d42d23d0815cc63bc16670d15e26
momodding <utsman.fajar@gmail.com>
2024-02-19 09:08:29 +0700

    fix sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">CBT TPA</a>
+            <a href="index.html">POS Resto</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">CT</a>
+            <a href="index.html">POS</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
@@ -25,7 +25,7 @@
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Users</span></a>
                 <ul class="dropdown-menu">
                     <li '>
-                        <a class="nav-link" href="{{ route(' users.index') }}">All Users</a>
+                        <a class="nav-link" href="{{ route('users.index') }}">All Users</a>
                     </li>
 
                 </ul>
